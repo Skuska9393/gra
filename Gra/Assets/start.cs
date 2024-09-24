8b2a5329608420c436a9fbdbe5e138b5c2e9048e
Skuska9393 <jakub.pecherski@gmail.com>
2024-09-24 18:13:51 +0200

    menu główne

---
+++ Gra/Assets/start.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("NazwaTwojejSceny"); // podaj nazwę sceny gry
+    }
+
+    public void ShowAbout()
+    {
+        // Możesz wyświetlić okno "O nas" lub załadować scenę z informacjami
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit(); // Tylko działa na urządzeniu
+    }
+}
