a4e15afde0f3dc1b8d47f74ce7e33a70fd681a8a
Mfurmanska <furmanska.marta@gmail.com>
2024-10-27 15:21:52 +0100

    Próba zrobienia puzzli

--- Gra/Assets/Puzzle.cs
+++ Gra/Assets/Puzzle.cs
@@ -1,20 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.EditorTools;
 using UnityEngine;
-
-asdasdasd;
 
 public class Puzzle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
+        if (!GameObjectToolContext.youWin)
+            transform.Rotate(0f, 0f, 90f);
+    }
         
     }
 }
