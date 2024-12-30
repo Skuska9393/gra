58f8518c8ed83d789734cda18cb2c6f3153894d8
Skuska9393 <jakub.pecherski@gmail.com>
2024-12-30 12:38:28 +0100

    poprawki

---
+++ Gra/Assets/wyborgry.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController1 : MonoBehaviour
+{
+    public void Gra1()
+    {
+        SceneManager.LoadScene("segregowaniesmieci"); // podaj nazwę sceny gry
+    }
+
+    public void Gra2()
+    {
+         SceneManager.LoadScene("segregowaniesmieci"); // podaj nazwę sceny gry
+    }
+
+   
+   
+}
